28175493a443fc58d4dd267053ddb1940465a10d
Luís Felipe Melo <luisfelipemelo56@gmail.com>
2024-09-09 22:17:27 -0300

    Alterações no Result.Escolar com condições encadeadas

--- src/ResultadoEscolar.java
+++ src/ResultadoEscolar.java
@@ -6,6 +6,9 @@ public class ResultadoEscolar {
         if (nota >= 6)
          System.out.println("Parabéns! Você foi aprovado.");
 
+        else if (nota >= 5 && nota < 7)
+         System.out.println("Direcionado à recuperação.");
+
         else 
          System.out.println("Infelizmente, você foi reprovado.");
     }
