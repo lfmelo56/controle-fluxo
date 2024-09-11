a929654bd9780f9a8fa55c2abf6d711d92c8ae66
Luís Felipe Melo <luisfelipemelo56@gmail.com>
2024-09-10 21:47:02 -0300

    Mais usos de condicionais, dessa vez com sistema de tamanhos

---
+++ src/SistemaMedidas.java
@@ -0,0 +1,17 @@
+public class SistemaMedidas {
+    public static void main(String[] args) {
+        String sigla = "P";
+
+        if(sigla == "P")
+         System.out.println("Pequeno");
+
+        else if (sigla == "M")
+         System.out.println("Médio");
+
+        else if (sigla == "G")
+         System.out.println("Médio");
+
+        else
+        System.out.println("TAMANHO INDEFINIDO"); 
+    }
+}
